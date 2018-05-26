3e242ea2378728d13baba84a1c471a5230c5efa6
KucharskiPiotr <piotr-kucharski@outlook.com>
2018-05-26 23:05:23 +0200

    asdf

--- LetsDoSomething/main.cpp
+++ LetsDoSomething/main.cpp
@@ -2,7 +2,7 @@
 and may not be redistributed without written permission.*/
 
 //Using SDL and standard IO
-#include <SDL.h>
+#include <SDL2/SDL.h>
 #include <stdio.h>
 
 //Screen dimension constants
@@ -22,7 +22,7 @@ int main(int argc, char* args[])
 	canvas = SDL_LoadBMP("lel.bmp");
 	if (canvas == nullptr)
 	{
-
+		
 	}
 
 	//Initialize SDL
